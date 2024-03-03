f994be7eb93de0a8dcb05b3b48f037f9e20e5be6
k35tan <k35tan@uwaterloo.ca>
2024-03-03 16:34:07 -0500

    adding login screen and friends screen
    - changed user id to mutable int (should be able to log in and out, need to be able to modify the id)
    - added a friend model and mock friend data

--- app/src/main/java/com/example/cityspots/view/AppNavigator.kt
+++ app/src/main/java/com/example/cityspots/view/AppNavigator.kt
@@ -9,14 +9,16 @@ import com.example.cityspots.viewmodel.UserViewModel
 @Composable
 fun AppNavigator(userViewModel: UserViewModel) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "home") {
-//        composable("login") {
-//            LoginScreen(navController) { userId ->
-//                userViewModel.loginUser(userId) // Update the logged-in user in ViewModel
-//            }
-//        }
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(navController) { userId ->
+                userViewModel.loginUser(userId) // Update the logged-in user in ViewModel
+            }
+        }
         composable("home") { HomeScreen(navController, userViewModel) }
         composable("ranking") { RankingScreen(navController, userViewModel) }
         composable("addEntry") { AddEntryScreen(navController) }
+        composable("friends") { FriendsScreen(navController, userViewModel) }
+
     }
 }
