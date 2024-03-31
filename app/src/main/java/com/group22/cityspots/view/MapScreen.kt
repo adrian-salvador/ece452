b8f9b1d70d0051a910de874723b7a2fe55658ad9
Gallost <harryhe187@gmail.com>
2024-03-31 16:11:36 -0400

    Improve map search bar styling and change the ui for saving location

--- app/src/main/java/com/group22/cityspots/view/MapScreen.kt
+++ app/src/main/java/com/group22/cityspots/view/MapScreen.kt
@@ -1,6 +1,7 @@
 package com.group22.cityspots.view
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,12 +14,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,8 +33,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
@@ -38,6 +47,7 @@ import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.group22.cityspots.viewmodel.MapViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen(
     viewModel: MapViewModel,
@@ -47,9 +57,10 @@ fun MapScreen(
     val cameraPositionState = rememberCameraPositionState{
         position = CameraPosition.fromLatLngZoom(
             viewModel.currentLatLong,
-            18f
+            13f
         )
     }
+    var searchBarFocused by remember { mutableStateOf(false) }
     LaunchedEffect(viewModel.currentLatLong) {
         cameraPositionState.animate(CameraUpdateFactory.newLatLng(viewModel.currentLatLong))
     }
@@ -74,25 +85,39 @@ fun MapScreen(
             Surface(
                 modifier = Modifier
                     .align(Alignment.TopCenter)
-                    .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 8.dp)
+                    .padding(16.dp)
                     .fillMaxWidth(),
-                color = Color.White,
-                shape = RoundedCornerShape(8.dp)
+                color = Color.Transparent,
+                shape = RoundedCornerShape(8.dp),
+                shadowElevation = 10.dp
             ) {
                 Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .background(Color.White)
                 ) {
                     var text by remember { mutableStateOf("") }
-                    OutlinedTextField(
+                    TextField(
                         value = text,
+                        label = { Text(
+                            "Search for a location",
+                            style = TextStyle(
+                                fontSize = if (searchBarFocused) 12.sp else 14.sp
+                            )
+                        ) },
+                        textStyle = TextStyle(
+                            fontSize = 14.sp
+                        ),
+                        colors = TextFieldDefaults.colors(unfocusedContainerColor = Color.White, focusedContainerColor = Color.White, unfocusedIndicatorColor = Color.Transparent, focusedIndicatorColor = Color.Transparent),
                         onValueChange = {
                             text = it
                             viewModel.searchPlaces(it)
                         },
                         modifier = Modifier
                             .fillMaxWidth()
+                            .onFocusChanged {
+                                searchBarFocused = it.isFocused
+                            }
                     )
                     AnimatedVisibility(
                         viewModel.locationAutofill.isNotEmpty(),
@@ -128,11 +153,11 @@ fun MapScreen(
                     contentColor = Color.Black
                 ),
                 modifier = Modifier
-                    .padding(5.dp)
-                    .align(Alignment.BottomStart),
+                    .padding(bottom = 10.dp)
+                    .align(Alignment.BottomCenter),
                 contentPadding = PaddingValues(6.dp)
             ) {
-                Text(text = "Back")
+                Icon(Icons.Filled.Check, "Save Location")
             }
         }
     }
