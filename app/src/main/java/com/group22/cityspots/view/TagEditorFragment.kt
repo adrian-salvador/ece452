7415c98e7671932ddd4747b564ff03ce1b367e9c
Gallost <harryhe187@gmail.com>
2024-03-31 21:15:54 -0400

    Add recommended tags

--- app/src/main/java/com/group22/cityspots/view/TagEditorFragment.kt
+++ app/src/main/java/com/group22/cityspots/view/TagEditorFragment.kt
@@ -1,6 +1,5 @@
 package com.group22.cityspots.view
 
-import android.view.KeyEvent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -25,6 +26,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -44,6 +46,8 @@ fun TagEditorFragment(
     removeTag: (String) -> Unit
 ) {
     var newTag by remember { mutableStateOf("") }
+    val _recommendedTags = listOf("Good for locals", "Good for tourists", "Safe", "Dangerous")
+    val recommendedTags = remember { mutableStateListOf<String>(*_recommendedTags.toTypedArray()) }
     Column {
         TextField(
             value = newTag,
@@ -93,6 +97,14 @@ fun TagEditorFragment(
             ),
         )
         Spacer(modifier = Modifier.height(10.dp))
+        LazyRow {
+            itemsIndexed(recommendedTags.sorted()) { _i, tag ->
+                Tag(tag = tag, isRecommendedTag = true, addTag = addTag, removeRecommendedTag = {
+                    recommendedTags.remove(tag)
+                })
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
         if (tags.value?.isNotEmpty() == true){
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -102,34 +114,55 @@ fun TagEditorFragment(
                     .height(30.dp)
             ){
                 tags.value?.forEach { tag ->
-                    Surface(
-                        shape = RoundedCornerShape(16.dp),
-                        color = Color(0xFFDBE8F9),
-                        contentColor = Color(0xFF176FF2),
-                        modifier = Modifier.clickable { removeTag(tag) }
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier.padding(
-                                start = 10.dp,
-                                end = 5.dp,
-                                top = 5.dp,
-                                bottom = 5.dp
-                            )
-                        ) {
-                            Text(
-                                text = tag,
-                                style = TextStyle(fontWeight = FontWeight.Bold),
-                            )
-                            Icon(
-                                imageVector = Icons.Filled.Clear,
-                                contentDescription = null,
-                                modifier = Modifier.size(15.dp),
-                            )
-                        }
-                    }
+                    Tag(tag = tag, removeTag = removeTag, addRecommendedTag = {
+                        if (_recommendedTags.contains(tag)) recommendedTags.add(tag)
+                    })
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun Tag(
+    tag: String,
+    isRecommendedTag: Boolean = false,
+    removeTag: (String) -> Unit = {}, addTag: (String) -> Unit = {},
+    removeRecommendedTag: (String) -> Unit = {}, addRecommendedTag: (String) -> Unit = {}
+) {
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = Color(0xFFDBE8F9),
+        contentColor = Color(0xFF176FF2),
+        modifier = Modifier.clickable {
+            if (isRecommendedTag) {
+                addTag(tag)
+                removeRecommendedTag(tag)
+            }
+            else {
+                removeTag(tag)
+                addRecommendedTag(tag)
+            }
+        }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(
+                start = 10.dp,
+                end = 5.dp,
+                top = 5.dp,
+                bottom = 5.dp
+            )
+        ) {
+            Text(
+                text = tag,
+                style = TextStyle(fontWeight = FontWeight.Bold),
+            )
+            Icon(
+                imageVector = if (isRecommendedTag) Icons.Filled.Add else Icons.Filled.Clear,
+                contentDescription = null,
+                modifier = Modifier.size(15.dp),
+            )
         }
     }
 }
